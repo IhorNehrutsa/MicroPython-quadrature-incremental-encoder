b457903250ab7bb5d498b11c9290a908c2810fa7
IhorNehrutsa <Ihor.Nehrutsa@gmail.com>
2021-09-29 22:25:47 +0300

    Correct Encoder class API according to Peter Hinch peterhinc https://github.com/peterhinch/micropython-samples/blob/master/encoders/encoder_portable.py

--- quad_irq.py
+++ quad_irq.py
@@ -33,43 +33,34 @@ class Debounce():
         return self.stable_value
     
 
-class ClockMultiplier:
-    x1 = 1
-    x2 = 2
-    x4 = 4
-
-
-class QUAD():
-    _x1 = (0, 0, 1, 0, 0, 0, 0, 0, -1, 0, 0, 0, 0, 0, 0, 0) 
-    _x2 = (0, 0, 1, 0, 0, 0, 0, -1, -1, 0, 0, 0, 0, 1, 0, 0)
-    _x4 = (0, -1, 1, 0, 1, 0, 0, -1, -1, 0, 0, 1, 0, 1, -1, 0)
-
-    def __init__(self, a, b, ppr=30, x124=ClockMultiplier.x2, c=None):
+class Encoder():
+    def __init__(self, a, b, ppr=30, x124=2, scale=1, c=None):
         self.a = a
         self.b = b
-        self.x124 = x124
-        
-        self.c = c
         self.ppr = ppr
+        self.x124 = x124
+        self.scale = scale
+
+        self.c = c
         self._rps = 0
         
-        self.dir = 0
+        self._dir = 0
 
-        self._count = 0
+        self._counter = 0
         self._state = 0
         self._time = ticks_us()
 
-        if x124 == ClockMultiplier.x1:
-            self._x = self._x1
-        elif x124 == ClockMultiplier.x2:
-            self._x = self._x2
-        elif x124 == ClockMultiplier.x4:
-            self._x = self._x4
+        if x124 == 1:
+            self._x = (0, 0, 1, 0, 0, 0, 0, 0, -1, 0, 0, 0, 0, 0, 0, 0) 
+        elif x124 == 2:
+            self._x = (0, 0, 1, 0, 0, 0, 0, -1, -1, 0, 0, 0, 0, 1, 0, 0)
+        elif x124 == 4:
+            self._x = (0, -1, 1, 0, 1, 0, 0, -1, -1, 0, 0, 1, 0, 1, -1, 0)
         else:
             raise ValueError("x124 must be from [1, 2, 4]")
 
-        self.c_debounced_value = None
         if c is not None:
+            self.c_debounced_value = None
             self.c_debounce = Debounce(c)
             self.c_debounced_value = self.c_debounce.value()
             self.c.irq(self.Isr)
@@ -92,42 +83,54 @@ class QUAD():
             pass
     
     def __del__(self):
-        self.deinit()
+        self.deinit()  
 
     def __repr__(self): 
-        ret = 'QUAD(A={}, B={}, x124={}'.format(self.a, self.b, self.x124)
+        ret = 'Encoder(A={}, B={}, mppr={}, x124={}, scale={}'.format(self.a, self.b, self.ppr, self.x124, self.scale)
         if self.c is not None:
-            ret += ', C={}, ppr={}'.format(self.c, self.ppr)
+            ret += ', C={}'.format(self.c)
         return ret + ')'
     
     def Isr(self, pin):
-        c_debounced_value = self.c_debounce.value()
+        _time = ticks_us()
+        if self.c is not None:
+            c_debounced_value = self.c_debounce.value()
         if (pin == self.a) or (pin == self.b):
             self._state = ((self._state << 2) + (self.b.value() << 1) + self.a.value()) & 0xF
-            self.dir = self._x[self._state]
-            self._count += self.dir
-            if (pin == self.b) and (self.dir != 0):
-                _time = ticks_us()
+            _dir = self._x[self._state]
+            self._counter += _dir
+            if (pin == self.b) and (_dir != 0):
+                self._dir = _dir
                 dt = ticks_diff(_time, self._time)
                 self._time = _time
                 if dt > 0:
-                    self._rps = self.dir * 1_000_000 / (self.ppr * self.x124 * dt)
+                    self._rps = _dir * 1_000_000 / (self.ppr * self.x124 * dt)
 
-        if self.c_debounced_value == c_debounced_value:
-            if self.c.value() == 0:
-                self._count = self.ppr * round(self._count / self.ppr)
-        else:
-            self.c_debounced_value = c_debounced_value
+        if self.c is not None:
+            if self.c_debounced_value == c_debounced_value:
+                if self.c.value() == 0:
+                    self._counter = self.ppr * round(self._counter / self.ppr)
+            else:
+                self.c_debounced_value = c_debounced_value
+            
+    def dir(self):
+        _dir = self._dir
+        self._dir = 0
+        return _dir
 
-    def count(self):
-        return self._count
+    def counter(self):
+        return self._counter
  
-    def set_count(self, count):
-        self._count = count
+    def set_counter(self, counter):
+        self._counter = counter
 
     def rps(self):
         return self._rps
         
     def rpm(self):
         return self._rps * 60
-    
+
+    def position(self, value=None):
+        if value is not None:
+            self._counter = value // self.scale
+        return self._counter * self.scale
--- quad_irq_test.py
+++ quad_irq_test.py
@@ -1,19 +1,19 @@
 # quad_irq_test.py
 
 from time import sleep
-try:
-    from machine import Pin
-except ImportError:
-    from pyb import Pin
+from machine import Pin
 
+# YOU MAY USE
 from quad_irq import *
+# OR
+#from encoder_portable import *
 
 #Encoder
 ENCODER_DATA = 17  # yellow
 ENCODER_CLK = 16  # green
-ENCODER_SW = 18 # orange
+ENCODER_SW = 18  # orange
 
-CORRECT_COUNTER = False
+CORRECT_COUNTER = True  # False
 
 try:    
     data = Pin(ENCODER_DATA, mode=Pin.IN, pull=Pin.PULL_UP)
@@ -23,33 +23,36 @@ try:
     print('clk', clk, clk.value())
     print('sw', sw, sw.value())
 
-    #enc1 = QUAD(data, clk, ppr=15, x124=ClockMultiplier.x1, c=sw)
-    enc1 = QUAD(data, clk, ppr=30, x124=ClockMultiplier.x2, c=sw)
-    #enc1 = QUAD(data, clk, ppr=60, x124=ClockMultiplier.x4) #, c=sw)
+    #enc1 = Encoder(data, clk, ppr=15, x124=1, c=sw)
+    #enc1 = Encoder(data, clk, ppr=30, x124=2)#, c=sw)
+    #enc1 = Encoder(data, clk, ppr=60, x124=4) #, c=sw)
+    enc1 = Encoder(data, clk)
     print(enc1)
 
     _data = None
     _clk = None
     _sw = None
-    _count = None
+    _counter = None
 
     while True:
         __data = data.value()
         __clk = clk.value()
         __sw = sw.value()
-        __count = enc1.count()
+        __counter = enc1.position()  #enc1.counter()
         
-        if (_data != __data) or (_clk != __clk) or (_sw != __sw)  or (_count != __count):
+        if (_data != __data) or (_clk != __clk) or (_sw != __sw)  or (_counter != __counter):
             _data = __data
             _clk = __clk
             _sw = __sw
-            _count = __count
+            _counter = __counter
 
             if CORRECT_COUNTER:
                 if _sw == 0:
-                    enc1.set_count(enc1.ppr * round(enc1.count() / enc1.ppr))
+                    # enc1.set_counter(enc1.ppr * round(enc1.counter() / enc1.ppr))
+                    enc1.position(0)
 
-            print("enc1={}, dir={}, rps={:10.3f}, data={}, clk={}, sw={} {}".format(_count, enc1.dir, enc1.rps(), _data, _clk, _sw, enc1.c_debounced_value), end='        \r')
+            #print("enc1={}, dir={}, rps={:10.3f}, data={}, clk={}, sw={}".format(_counter, enc1.dir(), enc1.rps(), _data, _clk, _sw), end='        \r')
+            print("enc1={}, data={}, clk={}, sw={}".format(_counter, _data, _clk, _sw), end='        \r')
             
         sleep(0.1)
 
